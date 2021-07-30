948d694d22478101546b7e7e6b24ba2b890d81e9
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-07-30 23:53:27 +0100

    defined the folder structure for the resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,3 +1,19 @@
-const resolvers = {};
+const events = require("./events");
+const user = require("./user");
+const createEventItemInput = require("./createEventItemInput");
+const updateEventItemInput = require("./updateEventItemInput");
+const createUserInput = "./createUserInput.js";
+
+const resolvers = {
+  Query: {
+    events,
+    user,
+  },
+  Mutation: {
+    createEventItemInput,
+    updateEventItemInput,
+    createUserInput,
+  },
+};
 
 module.exports = resolvers;
