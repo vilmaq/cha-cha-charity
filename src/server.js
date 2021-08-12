457a84c4831b682f764947e529d5e7020501a3ab
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-08-12 22:37:19 +0100

    added a context folder

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const db = require("./config/connection");
 
 const resolvers = require("./resolvers");
 const typeDefs = require("./schema");
+const context = require("./context");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
 });
 
 db.once("open", () => {
