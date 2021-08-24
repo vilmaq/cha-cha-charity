d9e9b0c87e58b5be8126f7769ce619a0616adc13
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-08-24 13:22:57 +0100

    modified create event to match the model

--- src/resolvers/createEvent.js
+++ src/resolvers/createEvent.js
@@ -1,71 +1,70 @@
-// const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError } = require("apollo-server");
 
-// const { Event, User } = require("../models");
+const { Event, User } = require("../models");
 
-// const createEvent = async (_, { input }, context) => {
-//   if (context.user) {
-//     const {
-//       name,
-//       fullName,
-//       password,
-//       imageUrl,
-//       street,
-//       phoneNumber,
-//       postcode,
-//       city,
-//       country,
-//       email,
-//       bio,
-//       animals,
-//       environmental,
-//       international,
-//       health,
-//       education,
-//       artCulture,
-//     } = input;
+const createEvent = async (_, { input }, context) => {
+  if (context.user) {
+    const {
+      name,
+      fullName,
+      password,
+      imageUrl,
+      street,
+      phoneNumber,
+      postcode,
+      city,
+      country,
+      email,
+      bio,
+      animals,
+      environmental,
+      international,
+      health,
+      education,
+      artCulture,
+    } = input;
 
-//     const user = await User.create({
-//       name: [],
-//       last_name: [],
-//       password: [],
-//       imageUrl: [],
-//       street: [],
-//       phone_number: [],
-//       postcode: [],
-//       city: [],
-//       country: [],
-//       email: [],
-//       socials: [],
-//       bio: [],
-//       animals: [],
-//       environmental: [],
-//       international: [],
-//       health: [],
-//       education: [],
-//       art_culture: [],
-//     });
+    const user = await User.create({
+      name: [],
+      fullName: [],
+      password: [],
+      imageUrl: [],
+      street: [],
+      phoneNumber: [],
+      postcode: [],
+      city: [],
+      country: [],
+      email: [],
+      bio: [],
+      animals: [],
+      environmental: [],
+      international: [],
+      health: [],
+      education: [],
+      artCulture: [],
+    });
 
-//     return await Event.create({
-//       name,
-//       day,
-//       postCode,
-//       street,
-//       city,
-//       country,
-//       organizer,
-//       creator,
-//       imageUrl,
-//       description,
-//       user,
-//       user: context.user.id,
-//     });
-//   } else {
-//     throw new AuthenticationError("Not authorized");
-//   }
-// };
+    return await Event.create({
+      name,
+      day,
+      postCode,
+      street,
+      city,
+      country,
+      organizer,
+      creator,
+      imageUrl,
+      description,
+      user,
+      user: context.user.id,
+    });
+  } else {
+    throw new AuthenticationError("Not authorized");
+  }
+};
 
-// // id: "123",
-// // title: "New Event",
-// // content: "This is a new Event!!",
+// id: "123",
+// title: "New Event",
+// content: "This is a new Event!!",
 
-// module.exports = createEvent;
+module.exports = createEvent;
