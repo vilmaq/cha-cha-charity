7c3cebd90b4018be3f0ccb0b0913bfcb45c3eeca
Misha <73886737+misha244@users.noreply.github.com>
2021-08-25 10:40:57 +0100

    seeded businesses

--- src/data/events.js
+++ src/data/events.js
@@ -1,19 +1,5 @@
 const events = [
-  {
-    type: "Environmental",
-    name: "Photography workshop by Aspire West Sussex",
-    description:
-      "Join Aspire West Sussex for a bitesize workshop on 'Photography: Framing Petworth - Pattern and Form' in the beautiful setting of Petworth Park. The 'Framing Petworth' series of field photography workshops provide a photographic exploration of the stunning grounds of Petworth House, following a variety of creative themes. Workshops are designed to be enjoyable to all, regardless of your level of photographic experience, knowledge or camera-type.",
-    day: "2021-09-02",
-    street: "Petworth House and Park",
-    postcode: "GU28 9LR",
-    city: "Petworth",
-    country: "United Kingdom",
-    organizer: "National Trust - Petworth House and Park",
-    imageUrl:
-      "https://images.unsplash.com/photo-1500027419059-afa3360ead5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-    participants: [],
-  },
+  /// ANIMALS
   {
     type: "Animals",
     name: "Fun Dog Show",
@@ -29,64 +15,21 @@ const events = [
       "https://images.unsplash.com/photo-1494947665470-20322015e3a8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     participants: [],
   },
-  {
-    type: "Education",
-    name: "The Brilliant Breakfast 2021",
-    description:
-      "During the week of 11 – 17 October 2021, start your day with a Brilliant Breakfast that will change lives by raising money for young women in the UK who need our support, helping them wake up to a more positive futurThe Brilliant Breakfast is open to everyone to join in – it could be with friends round your kitchen table, with clients or colleagues in your office, with customers in your shops or virtually via Zoom or social media!To support the campaign, please contact the Events team on brilliantbreakfast@princes-trust.org.uk or call 020 7543 1370.Thank you to all of the companies who are supporting The Brilliant Breakfast this year.",
-    day: "2021-10-02",
-    street: "New Street",
-    postcode: "B2 3DF",
-    city: "Birmingham",
-    country: "United Kingdom",
-    organizer: "The Prince’s Trust",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-    participants: [],
-  },
-  {
-    type: "Art_Culture",
-    name: "The Way Ahead",
-    description:
-      "The Way Ahead was originally intended to be a 6-month community art project but 17 years later it is still in demand as a disability art protest piece which clearly demonstrates there is much more to be done, and much more to say. The rules of the road are a wonderful visual system. Information is easily communicated through the arrangement of simple shapes and symbols. When it comes to travel, we have found a system most people can understand easily. The Way Ahead was inspired by a wish that such a system could exist for disability awareness. This exhibition has been co-commissioned with Shape Arts. ",
-    day: "2021-10-02",
-    street: "Barnsley Civic, Hanson St,",
-    postcode: "S70 2HZ",
-    city: "Barnsley",
-    country: "United Kingdom",
-    organizer: "Shape Arts",
-    imageUrl:
-      "https://images.unsplash.com/photo-1566954979172-eaba308acdf0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80",
-    participants: [],
-  },
+
+  /// ENVIRONMENTAL
   {
     type: "Environmental",
-    name: "Community Tree Planting",
+    name: "Photography workshop by Aspire West Sussex",
     description:
-      "National Tree Week is the UK’s largest annual tree celebration, marking the start of the winter tree planting season (November to March each year). In 2021, it will take place from Saturday 27 November – Sunday 5 December. We look forward to bringing people together to celebrate and plant trees across the country and hope to bring you another inspiring online programme of tree-related arts and culture events.",
-    day: "2021-11-27",
-    street: "The Ridgeway",
-    postcode: "EN2 8AP",
-    city: "Enfield",
+      "Join Aspire West Sussex for a bitesize workshop on 'Photography: Framing Petworth - Pattern and Form' in the beautiful setting of Petworth Park. The 'Framing Petworth' series of field photography workshops provide a photographic exploration of the stunning grounds of Petworth House, following a variety of creative themes. Workshops are designed to be enjoyable to all, regardless of your level of photographic experience, knowledge or camera-type.",
+    day: "2021-09-02",
+    street: "Petworth House and Park",
+    postcode: "GU28 9LR",
+    city: "Petworth",
     country: "United Kingdom",
-    organizer: "Friends of the Earth",
+    organizer: "National Trust - Petworth House and Park",
     imageUrl:
-      "https://images.unsplash.com/photo-1524602585632-a2a01c12307c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1982&q=80",
-    participants: [],
-  },
-  {
-    type: "Health",
-    name: "Cheltenham 10k 2021",
-    description:
-      "New for 2021, this Cheltenham 10km route will start and finish at the stunning Cheltenham Racecourse on the outskirts of this beautiful regency town. Taking place alongside and following some of the (closed road) route of the popular Cheltenham Half Marathon, 10km runners will enjoy glorious views of the surrounding Cotswolds before heading off along tree-lined roads towards the stunning Town Centre, passing through the iconic Pitville Park. This event is a truly amazing opportunity to see the best Cheltenham has to offer.  So whether you’re looking to increase your fitness, beat your personal best, or push yourself to the limit for charity, this is a fantastic opportunity.By joining #TeamAcorns you will be raising money for life-limited and life-threatened babies, children, and young people across Gloucestershire, Worcestershire, and the wider West Midlands region.",
-    day: "2021-09-05",
-    street: "Cheltenham Racecourse",
-    postcode: "GL50 4SH",
-    city: "Cheltenham",
-    country: "United Kingdom",
-    organizer: "Acorns Children's Hospice",
-    imageUrl:
-      "https://images.unsplash.com/photo-1590333748338-d629e4564ad9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1418&q=80",
+      "https://images.unsplash.com/photo-1500027419059-afa3360ead5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     participants: [],
   },
   {
@@ -106,9 +49,78 @@ const events = [
   },
   {
     type: "Environmental",
-    name: "Placeholder Event",
+    name: "Community Tree Planting",
     description:
-      "I've added this event to add users to while I'm seeding charities and businesses",
+      "National Tree Week is the UK’s largest annual tree celebration, marking the start of the winter tree planting season (November to March each year). In 2021, it will take place from Saturday 27 November – Sunday 5 December. We look forward to bringing people together to celebrate and plant trees across the country and hope to bring you another inspiring online programme of tree-related arts and culture events.",
+    day: "2021-11-27",
+    street: "The Ridgeway",
+    postcode: "EN2 8AP",
+    city: "Enfield",
+    country: "United Kingdom",
+    organizer: "Friends of the Earth",
+    imageUrl:
+      "https://images.unsplash.com/photo-1524602585632-a2a01c12307c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1982&q=80",
+    participants: [],
+  },
+
+  /// INTERNATIONAL
+
+  /// HEALTH
+  {
+    type: "Health",
+    name: "Cheltenham 10k 2021",
+    description:
+      "New for 2021, this Cheltenham 10km route will start and finish at the stunning Cheltenham Racecourse on the outskirts of this beautiful regency town. Taking place alongside and following some of the (closed road) route of the popular Cheltenham Half Marathon, 10km runners will enjoy glorious views of the surrounding Cotswolds before heading off along tree-lined roads towards the stunning Town Centre, passing through the iconic Pitville Park. This event is a truly amazing opportunity to see the best Cheltenham has to offer.  So whether you’re looking to increase your fitness, beat your personal best, or push yourself to the limit for charity, this is a fantastic opportunity.By joining #TeamAcorns you will be raising money for life-limited and life-threatened babies, children, and young people across Gloucestershire, Worcestershire, and the wider West Midlands region.",
+    day: "2021-09-05",
+    street: "Cheltenham Racecourse",
+    postcode: "GL50 4SH",
+    city: "Cheltenham",
+    country: "United Kingdom",
+    organizer: "Acorns Children's Hospice",
+    imageUrl:
+      "https://images.unsplash.com/photo-1590333748338-d629e4564ad9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1418&q=80",
+    participants: [],
+  },
+
+  /// EDUCATION
+
+  {
+    type: "Education",
+    name: "The Brilliant Breakfast 2021",
+    description:
+      "During the week of 11 – 17 October 2021, start your day with a Brilliant Breakfast that will change lives by raising money for young women in the UK who need our support, helping them wake up to a more positive futurThe Brilliant Breakfast is open to everyone to join in – it could be with friends round your kitchen table, with clients or colleagues in your office, with customers in your shops or virtually via Zoom or social media!To support the campaign, please contact the Events team on brilliantbreakfast@princes-trust.org.uk or call 020 7543 1370.Thank you to all of the companies who are supporting The Brilliant Breakfast this year.",
+    day: "2021-10-02",
+    street: "New Street",
+    postcode: "B2 3DF",
+    city: "Birmingham",
+    country: "United Kingdom",
+    organizer: "The Prince’s Trust",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    participants: [],
+  },
+  /// ART_CULTURE
+  {
+    type: "Art_Culture",
+    name: "The Way Ahead",
+    description:
+      "The Way Ahead was originally intended to be a 6-month community art project but 17 years later it is still in demand as a disability art protest piece which clearly demonstrates there is much more to be done, and much more to say. The rules of the road are a wonderful visual system. Information is easily communicated through the arrangement of simple shapes and symbols. When it comes to travel, we have found a system most people can understand easily. The Way Ahead was inspired by a wish that such a system could exist for disability awareness. This exhibition has been co-commissioned with Shape Arts. ",
+    day: "2021-10-02",
+    street: "Barnsley Civic, Hanson St,",
+    postcode: "S70 2HZ",
+    city: "Barnsley",
+    country: "United Kingdom",
+    organizer: "Shape Arts",
+    imageUrl:
+      "https://images.unsplash.com/photo-1566954979172-eaba308acdf0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80",
+    participants: [],
+  },
+
+  {
+    type: "Art_Culture",
+    name: "Placeholder Event Charity",
+    description:
+      "I've added this event to add users to while I'm seeding charities",
     day: "2022-09-12",
     street: "3 River Brook Drive",
     postcode: "B30 2SH",
@@ -116,7 +128,22 @@ const events = [
     country: "United Kingdom",
     organizer: "Shrek",
     imageUrl:
-      "https://images.unsplash.com/photo-1594466179447-cd9892076389?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1990&q=80",
+      "https://i.kym-cdn.com/entries/icons/original/000/030/710/dd0.png",
+    participants: [],
+  },
+  {
+    type: "Art_Culture",
+    name: "Placeholder Event Business",
+    description:
+      "I've added this event to add users to while I'm seeding businesses",
+    day: "2021-09-05",
+    street: "Cheltenham Racecourse",
+    postcode: "GL50 4SH",
+    city: "Lake",
+    country: "United Kingdom",
+    organizer: "LeBron James",
+    imageUrl:
+      "https://i.kym-cdn.com/entries/icons/original/000/030/710/dd0.png",
     participants: [],
   },
 ];
