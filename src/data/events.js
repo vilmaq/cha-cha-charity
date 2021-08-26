6a09703dc9bf47b02857e0dfa5f574d7d332ea50
Misha <73886737+misha244@users.noreply.github.com>
2021-08-27 00:02:07 +0100

    edit

--- src/data/events.js
+++ src/data/events.js
@@ -5,8 +5,7 @@ const events = [
     name: "Fun Dog Show",
     description:
       "Join us again on Sunday 5th September 11.00am at Sandall Park to celebrate our wonderful pets! The RSPCA annual Fun Dog Show is back and it promises to be a woofully great day. Sponsored by Hannah's Happy Hounds the event will have our usual classes for your dog to enter, including: 'Smiliest Face','Waggiest Tail','Rescue Dog 2021','Junior Handler','Perfect Pup (under 1 year)','Young at Heart (7 - 11 years)','Golden Oldie (12 years +)','Bonniest Lass','Handsomest Lad','Best in Show'",
-    day: 2021 - 10 - 12,
-    time: 4,
+    day: "2021-10-12",
     street: "Thorne Road",
     postcode: "DN2 5DY",
     city: "Doncaster",
@@ -21,8 +20,7 @@ const events = [
     name: "Muddy Dog Challenge 2021",
     description:
       "Join us in Leeds for the Muddy Dog Challenge 2021, a 2.5km or 5km obstacle run that you can tackle with your dog. In the heart of Yorkshire sits the beautiful Harewood House. The parkland of the estate has views of the rolling landscape and is the venue of our Leeds Muddy Dog Challenge, which is less than 10 miles from Leeds, York and Harrogate. Whether you’re a fitness fanatic or just looking for a fun day out, keen to run, or would rather stroll, we welcome humans and their dogs to take part and raise money to help care for the dogs and cats at Battersea.The Muddy Dog Challenge is a 2.5K or 5K obstacle course that you can take on with your dog by your side. Tackle the course with friends and family, with or without a dog, and prepare yourself for a day of mud, sweat, and cheers! By taking part, you’ll be helping us provide love and care for the thousands of lost, abandoned and unwanted dogs and cats that come into our care.",
-    day: 2021 - 10 - 02,
-    time: 4,
+    day: "2021-10-02",
     street: "HAREWOOD HOUSE",
     postcode: "LS17 9LQ",
     city: "Leeds",
@@ -39,8 +37,7 @@ const events = [
     name: "Photography workshop by Aspire West Sussex",
     description:
       "Join Aspire West Sussex for a bitesize workshop on 'Photography: Framing Petworth - Pattern and Form' in the beautiful setting of Petworth Park. The 'Framing Petworth' series of field photography workshops provide a photographic exploration of the stunning grounds of Petworth House, following a variety of creative themes. Workshops are designed to be enjoyable to all, regardless of your level of photographic experience, knowledge or camera-type.",
-    day: 2021 - 09 - 02,
-    time: 4,
+    day: "2021-09-02",
     street: "Petworth House and Park",
     postcode: "GU28 9LR",
     city: "Petworth",
@@ -55,8 +52,7 @@ const events = [
     name: "Volunteer Litter Picking",
     description:
       "Join us for a spot of litter picking across the Ten Acres site. \n We will meet up outsdie riverbrook medical centre on the field. Children are welcome but keep little ones to the pathways due to the potential hazards in the woodland. \n\n(When restrictions ease you can just turn up but right now we need to ensure numbers are kept to keep restrictions) \n\nPlease let us know if you have your own litter picking tool (we have a limited supply for those that do not).",
-    day: 2022 - 09 - 12,
-    time: 4,
+    day: "2022-09-12",
     street: "3 River Brook Drive",
     postcode: "B30 2SH",
     city: "Stirchley",
@@ -71,8 +67,7 @@ const events = [
     name: "Community Tree Planting",
     description:
       "National Tree Week is the UK’s largest annual tree celebration, marking the start of the winter tree planting season (November to March each year). In 2021, it will take place from Saturday 27 November – Sunday 5 December. We look forward to bringing people together to celebrate and plant trees across the country and hope to bring you another inspiring online programme of tree-related arts and culture events.",
-    day: 2021 - 11 - 27,
-    time: 4,
+    day: "2021-11-27",
     street: "The Ridgeway",
     postcode: "EN2 8AP",
     city: "Enfield",
@@ -89,8 +84,7 @@ const events = [
     name: "Royal Parks Foundation Half Marathon",
     description:
       "The Royal Parks Half Marathon winds its way through four of London's eight Royal Parks - apply for a charity place today. Registration fee is only £5 and all we ask is for £350 fundraising pledge! ",
-    day: 2021 - 10 - 09,
-    time: 4,
+    day: "2021-10-09",
     street: "142 The Grove",
     postcode: "E15 1NS",
     city: "London",
@@ -105,8 +99,7 @@ const events = [
     name: "MSF Scientific Day",
     description:
       "MSF Scientific Days bring together researchers, practitioners, academics and patient representatives to catalyse improvements in the quality of care provided to patients and communities at risk.By supporting research and innovation in our projects, MSF aims to improve outcomes, find efficiencies and create a culture of best practice, constantly improving the standards of care provided to our patients.",
-    day: 2021 - 09 - 25,
-    time: 4,
+    day: "2021-09-25",
     street: "10 Furnival Street",
     postcode: "EC4A 1AB",
     city: "London",
@@ -123,8 +116,7 @@ const events = [
     name: "Cheltenham 10k 2021",
     description:
       "New for 2021, this Cheltenham 10km route will start and finish at the stunning Cheltenham Racecourse on the outskirts of this beautiful regency town. Taking place alongside and following some of the (closed road) route of the popular Cheltenham Half Marathon, 10km runners will enjoy glorious views of the surrounding Cotswolds before heading off along tree-lined roads towards the stunning Town Centre, passing through the iconic Pitville Park. This event is a truly amazing opportunity to see the best Cheltenham has to offer.  So whether you’re looking to increase your fitness, beat your personal best, or push yourself to the limit for charity, this is a fantastic opportunity.By joining #TeamAcorns you will be raising money for life-limited and life-threatened babies, children, and young people across Gloucestershire, Worcestershire, and the wider West Midlands region.",
-    day: 2021 - 09 - 05,
-    time: 4,
+    day: "2021-09-05",
     street: "Cheltenham Racecourse",
     postcode: "GL50 4SH",
     city: "Cheltenham",
@@ -139,8 +131,7 @@ const events = [
     name: "Acorns Children's Hospice Car Boot Sale",
     description:
       "Support your local hospice whilst selling your unwanted goods or finding yourself a bargain. Sellers admitted 8.30am. Buyers at 9.30am. Cars £9. Car and trailer or van £13. Buyers free entry and parking though donations are of course welcome. Every penny raised in donations and pitch fees goes straight into the hospice. Due to circumstances beyond our control we are also going to be looking for a couple of new volunteers to help run the car boot sales. If you think that you could help, whether it be for an odd Sunday or for a number of weeks over the season please do get in touch and ask for more details. In the event of bad weather updates will be posted on our Facebook page or you can ring 01482 784343.",
-    day: 2021 - 09 - 10,
-    time: 4,
+    day: "2021-09-10",
     street: "2300 The Crescent, Birmingham Business Park",
     postcode: "B37 7YE",
     city: "Birmingham",
@@ -152,16 +143,15 @@ const events = [
   },
   {
     type: "Health",
-    name: "Alzheimers UK's Shine Night Walk",
+    name: "Cancer Research UK's Shine Night Walk",
     description:
-      "Make 2021 a year to remember and experience a night like no other - a Marathon, Half Marathon or 10k charity night walk through your city. Raise money for life-saving research. It’s a night to celebrate, push yourself and feel proud. As always at our events the health and safety of our participants, staff and volunteers is our top priority. We will continue to monitor the situation closely and keep you updated with our planning, much of which will focus on how we deliver a safe event in the context of all necessary COVID-19 guidelines and precautions. It may be that events look a little different this year but we are working proactively with our venues and suppliers to deliver a great experience on site.",
-    day: 2021 - 10 - 03,
-    time: 4,
+      "Make 2021 a year to remember and experience a night like no other - a Marathon, Half Marathon or 10k charity night walk through your city. Select the cancer type you want to support and raise money for life-saving research. It’s a night to celebrate, push yourself and feel proud. As always at our events the health and safety of our participants, staff and volunteers is our top priority. We will continue to monitor the situation closely and keep you updated with our planning, much of which will focus on how we deliver a safe event in the context of all necessary COVID-19 guidelines and precautions. It may be that events look a little different this year but we are working proactively with our venues and suppliers to deliver a great experience on site.",
+    day: "2021-10-03",
     street: "62 Buckingham Rd",
     postcode: "L23 7RA",
     city: "Thornton",
     country: "UK",
-    organizer: "Alzheimer's Society",
+    organizer: "Cancer Research",
     imageUrl:
       "https://images.unsplash.com/photo-1580073288094-f676869b8ccf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1508&q=80",
     participants: [],
@@ -174,8 +164,7 @@ const events = [
     name: "The Brilliant Breakfast 2021",
     description:
       "During the week of 11 – 17 October 2021, start your day with a Brilliant Breakfast that will change lives by raising money for young women in the UK who need our support, helping them wake up to a more positive futurThe Brilliant Breakfast is open to everyone to join in – it could be with friends round your kitchen table, with clients or colleagues in your office, with customers in your shops or virtually via Zoom or social media!To support the campaign, please contact the Events team on brilliantbreakfast@princes-trust.org.uk or call 020 7543 1370.Thank you to all of the companies who are supporting The Brilliant Breakfast this year.",
-    day: 2021 - 10 - 02,
-    time: 4,
+    day: "2021-10-02",
     street: "New Street",
     postcode: "B2 3DF",
     city: "Birmingham",
@@ -190,8 +179,7 @@ const events = [
     name: "Bournemouth Half Marathon",
     description:
       "The Bournemouth Half Marathon course takes in both Boscombe and Bournemouth piers along this coastal route. The Bournemouth Half Marathon starts at the spectacular and beautiful Hengistbury Head, which forms the main division between Poole and Christchurch bays.  You will take in some spectacular views as you continue your coastal run. Make sure you take it all in before you start your sprint finish through your much-anticipated finish line on Bournemouth Pier Approach. Secure a free charity entry! Entry is free, but we ask that you pledge to raise a minimum sponsorship of £350.",
-    day: 2021 - 10 - 10,
-    time: 4,
+    day: "2021-10-10",
     street: "Hengistbury Head",
     postcode: "BH6 4EW",
     city: "Bournemouth",
@@ -207,8 +195,7 @@ const events = [
     name: "The Way Ahead",
     description:
       "The Way Ahead was originally intended to be a 6-month community art project but 17 years later it is still in demand as a disability art protest piece which clearly demonstrates there is much more to be done, and much more to say. The rules of the road are a wonderful visual system. Information is easily communicated through the arrangement of simple shapes and symbols. When it comes to travel, we have found a system most people can understand easily. The Way Ahead was inspired by a wish that such a system could exist for disability awareness. This exhibition has been co-commissioned with Shape Arts. ",
-    day: 2021 - 10 - 02,
-    time: 4,
+    day: "2021-10-02",
     street: "Barnsley Civic, Hanson St,",
     postcode: "S70 2HZ",
     city: "Barnsley",
@@ -220,16 +207,15 @@ const events = [
   },
   {
     type: "Art_Culture",
-    name: "Crafts Workshop Fundraiser",
+    name: "Placeholder Event Charity",
     description:
       "I've added this event to add users to while I'm seeding charities",
-    day: 2022 - 09 - 12,
-    time: 4,
+    day: "2022-09-12",
     street: "3 River Brook Drive",
     postcode: "B30 2SH",
     city: "Swamp",
     country: "United Kingdom",
-    organizer: "Art Fund",
+    organizer: "Shrek",
     imageUrl:
       "https://i.kym-cdn.com/entries/icons/original/000/030/710/dd0.png",
     participants: [],
@@ -239,13 +225,12 @@ const events = [
     name: "Placeholder Event Business",
     description:
       "I've added this event to add users to while I'm seeding businesses",
-    day: 2021 - 09 - 05,
-    time: 4,
+    day: "2021-09-05",
     street: "Cheltenham Racecourse",
     postcode: "GL50 4SH",
     city: "Lake",
     country: "United Kingdom",
-    organizer: "Art Fund",
+    organizer: "LeBron James",
     imageUrl:
       "https://i.kym-cdn.com/entries/icons/original/000/030/710/dd0.png",
     participants: [],
